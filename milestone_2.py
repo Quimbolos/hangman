c958b9db2eb36e5f2490cabc5a2c68a1618ad09f
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-08 00:25:49 +0000

    Task 4

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,21 @@
 # %% 
 import random
+import string # TASK 4
+
 
 guess = input("Enter your guess: ") # TASK 3
+
+### TASK 4
+
+alphabet = list(string.ascii_lowercase)
+
+if len(guess) == 1 and guess in alphabet:
+    print('Good guess!')
+
+else:
+    print('Oops! That is not a valid input.')
+
+###
 
 word_list = ['watermelon', 'apple', 'strawberries', 'blueberries', 'grapes']
 # print(word_list) - TASK 1
@@ -9,3 +23,5 @@ word_list = ['watermelon', 'apple', 'strawberries', 'blueberries', 'grapes']
 world = random.choice(word_list)
 # print(world) - TASK 2
 
+
+# %%
