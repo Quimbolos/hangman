2021edbfafdea803c0feb8d40f4876506fb2591d
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-08 10:24:35 +0000

    Task 2 - Milestone 3

--- milestone_3.py
+++ milestone_3.py
@@ -7,6 +7,8 @@ guess = input("Enter your guess: ")
 
 alphabet = list(string.ascii_lowercase)
 
+### TASK 1
+
 while len(guess) == 1 and guess in alphabet:
 
     break
@@ -14,12 +16,22 @@ while len(guess) == 1 and guess in alphabet:
 else:
     print('Invalid letter. Please, enter a single alphabetical character.')
 
-
+###
 
 word_list = ['watermelon', 'apple', 'strawberries', 'blueberries', 'grapes']
 # print(word_list) 
 
 world = random.choice(word_list)
 # print(world) 
+
+### TASK 2
+
+if guess in world:
+    print("Good guess!", guess,"is in the word")
+    
+else:
+    print("Sorry,", guess,"is not in this word")
+
+###
 
 # %%
