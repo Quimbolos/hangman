51a74216d9a97def02a61050fa420f0b6b5b5b95
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-08 00:17:55 +0000

    Task 3

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,11 @@
 # %% 
 import random
+
+guess = input("Enter your guess: ") # TASK 3
+
 word_list = ['watermelon', 'apple', 'strawberries', 'blueberries', 'grapes']
-print(word_list)
-# %%
+# print(word_list) - TASK 1
+
 world = random.choice(word_list)
-print(world)
-# %%
+# print(world) - TASK 2
+
