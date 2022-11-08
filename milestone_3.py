db38d5ca74bf5144cbe05b3def13729c61f50b40
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-08 17:31:23 +0000

    Task 1 - Milestone 3

--- milestone_3.py
+++ milestone_3.py
@@ -1,40 +1,13 @@
 # %% 
-import random
-import string 
-
-
-def check_guess(guess):
-
-    guess = guess.lower()
-
-    if guess in word:
-        print("Good guess!", guess,"is in the word")
-    
-    else:
-        print("Sorry,", guess,"is not in this word")
-
-
-def ask_for_input():
+while True:
 
     guess = input("Enter your guess: ") 
 
-    while len(guess) == 1 and guess in alphabet:
+    if len(guess) == 1 and guess.isalpha():
         break
 
     else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
-  
-    check_guess(guess)
-
-
-
-alphabet = list(string.ascii_lowercase)
-
-word_list = ['watermelon', 'apple', 'strawberries', 'blueberries', 'grapes']
-
-word = random.choice(word_list)
-
-ask_for_input()
+        print("Invalid letter. Please, enter a single alphabetical character.")
 
 
 
