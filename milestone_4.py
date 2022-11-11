66acd2347c2835fd551da6215fb07e083a936aa0
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-11 02:36:01 +0000

    Task 1 - Milestone 4

--- milestone_4.py
+++ milestone_4.py
@@ -3,10 +3,10 @@ import random
 
 class Hangman():
 
-    def __init__(self, wordlist, num_lives = 5):
-        self.wordlist = wordlist
+    def __init__(self, word_list, num_lives = 5):
+        self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(self.wordlist)
+        self.word = random.choice(self.word_list)
         self.word_guessed = ['_']*len(self.word)
         self.num_letters = len(self.word_guessed)
         self.list_of_guesses = ['']
